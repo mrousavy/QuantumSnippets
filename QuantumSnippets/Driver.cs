9f8fe51551185d1f7894c94d1787fc44c80df3ba
mrousavy <mrousavy@donotreply.github.com>
2017-12-13 18:53:16 +0100

    Create Solution

---
+++ QuantumSnippets/Driver.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Quantum.QuantumSnippets {
+    class Driver {
+        static void Main(string[] args) {
+
+        }
+    }
+}
